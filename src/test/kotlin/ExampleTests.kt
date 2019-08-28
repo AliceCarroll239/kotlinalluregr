f462e7b085e95dcebfb41afa9bbe0c46ecd93f82
AliceCarroll239 <dmutrynoraev@gmail.com>
2019-08-28 14:44:41 +0300

    some coroutine shit

--- src/test/kotlin/ExampleTests.kt
+++ src/test/kotlin/ExampleTests.kt
@@ -2,7 +2,7 @@ import com.google.gson.Gson
 import interfaces.GetMethodAsync
 import io.qameta.allure.Description
 import io.qameta.allure.Feature
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.*
@@ -11,12 +11,14 @@ import org.junit.jupiter.api.parallel.Execution
 import org.junit.jupiter.api.parallel.ExecutionMode
 import utils.DataProvider
 import utils.TestParams
+import java.util.concurrent.atomic.AtomicLong
+import org.assertj.core.api.ErrorCollector
 
 @DisplayName("Пример теста")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(OrderAnnotation::class)
 
-@Execution(ExecutionMode.CONCURRENT)
+//@Execution(ExecutionMode.CONCURRENT)
 
 class ExampleTests {
 
@@ -34,9 +36,32 @@ class ExampleTests {
         @DisplayName("GET")
         @Description("---")
         @Feature("Testing different HTTP verbs")
-        fun checkGetMethod() {
-            val result = stepsAgent.getMethod(testSettings)
-            assertThat(result!!.url, `is`("https://httpbin.org/get"))
+        fun checkGetMethodAsyncCorut() {
+            val count = AtomicLong()
+            runBlocking {
+                val result = (1..1000).map { n ->
+                    GlobalScope.async {
+                        try {
+                            assert(stepsAgent.getMethod(testSettings)!!.url.equals("https://httpbin.org/get"))
+                            //assertThat(stepsAgent.getMethod(testSettings)!!.url, `is`("https://httpbin.org/get"))
+                        } catch (e: java.lang.AssertionError) {
+                            count.incrementAndGet()
+                        }
+                    }
+                }
+                result.awaitAll()
+                println(count.get())
+            }
+        }
+
+        @Test
+        @DisplayName("GET")
+        @Description("---")
+        @Feature("Testing different HTTP verbs")
+        fun checkGetMethodNotAsync() {
+            for (i in 1..100) {
+                assertThat(stepsAgent.getMethod(testSettings)!!.url, `is`("https://httpbin.org/get"))
+            }
         }
 
         @Test
@@ -60,6 +85,7 @@ class ExampleTests {
                 override fun onRes(result: String) {
                     println(result)
                 }
+
                 override fun onResFailed() {
                     println("Тут упало")
                 }
